51eba865c6b02a6c6182dc03ec5b32c879b814fe
wrexlua <ahmetkaanakhan1980@gmail.com>
2025-04-09 18:59:25 +0300

    Added more ai functions

--- intelligence/ai.lua
+++ intelligence/ai.lua
@@ -1,1 +1,26 @@
-local ai = {}
+local ai = {
+  about {
+    _name = "ReBlox Intelligence"
+  }
+} -- Table AI
+table.freeze(ai.about) -- Anti-Edit Remote
+
+ai.data = nil -- is ai data?
+
+function ai.train(prompt: string) -- train function for ai
+  if prompt then -- is entered a prompt?
+    ai.data = prompt -- set data as prompt
+  else
+    return nil
+  end
+end
+
+function ai.ask(prompt: string)
+  if prompt then
+    return "Coming Soon!
+  else
+    return "Enter a prompt."
+  end
+end
+
+return ai
