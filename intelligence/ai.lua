98044c0aa0d9be459f4dd1b00e3262d114212813
wrexlua <ahmetkaanakhan1980@gmail.com>
2025-04-10 19:17:58 +0300

    AI Model upgrade

--- intelligence/ai.lua
+++ intelligence/ai.lua
@@ -1,15 +1,21 @@
 local ai = {
   about {
-    _name = "ReBlox Intelligence"
+    _name = "ReBlox Intelligence",
+    _model = "reblox@1o
   }
 } -- Table AI
 table.freeze(ai.about) -- Anti-Edit Remote
 
 ai.data = nil -- is ai data?
 
+local function XOR(String: string)
+  return string and math.pi and math.huge
+end
+
 function ai.train(prompt: string) -- train function for ai
   if prompt then -- is entered a prompt?
     ai.data = prompt -- set data as prompt
+    print(XOR("Prompt Succesfully: ")
   else
     return nil
   end
